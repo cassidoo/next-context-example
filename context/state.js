297dd12995a098961bb9dd357ea1e738fbac050a
Cassidy Williams <cassidoo@users.noreply.github.com>
2021-03-18 16:40:35 -0500

    Add reducer example

--- context/state.js
+++ context/state.js
@@ -1,10 +1,25 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useReducer } from 'react'
 
 const AppContext = createContext()
 
 export function AppWrapper({ children }) {
+  let initialState = {
+    darkMode: false,
+    name: ''
+  }
+
+  let reducer = (state, action) => {
+    if (action.type === "TOGGLE_DARK_MODE") {
+      return { ...state, darkMode: !state.darkMode }
+    } else if (action.type === "SET_NAME") {
+      return { ...state, name: action.value }
+    }
+  }
+
+  let [state, dispatch] = useReducer(reducer, initialState)
+
   return (
-    <AppContext.Provider value={{ fish: 'fishy' }}>
+    <AppContext.Provider value={{ state, dispatch }}>
       {children}
     </AppContext.Provider>
   )
--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,34 @@ import Footer from '@components/Footer'
 import { useAppContext } from '@context/state'
 
 export default function Home() {
-  let contextObj = useAppContext();
+  let {
+    state, dispatch
+  } = useAppContext();
 
-  console.log(contextObj)
+  let toggleDarkMode = () => {
+    dispatch({ type: "TOGGLE_DARK_MODE" })
+  }
 
   return (
-    <div className="container">
+    <div className={`container ${state.darkMode ? 'dark' : null}`}>
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Context object: {JSON.stringify(contextObj)}
-        </p>
+        <Header title={`Welcome ${state.name}`} />
+
+        <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
+      
+        <input type="text"
+               placeholder="Type your name!"
+               value={state.name} 
+               onChange={(event) => {
+                dispatch({ type: "SET_NAME", value: event.target.value })
+               }}
+        />
+      
       </main>
 
       <Footer />
